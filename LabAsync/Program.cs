69e9164106f9be245aa8a7626c9168ca0e4b844a
AntonZgriski <anton.zgirski@gmail.com>
2023-04-02 20:20:15 +0300

    async

---
+++ LabAsync/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace LabAsync
+{
+  internal class Program
+  {
+    static async Task Main(string[] args)
+    {
+      var cTs = new CancellationTokenSource();
+      cTs.Cancel();
+
+      var task = TestAsync(cTs.Token, 5000);
+
+      Console.WriteLine($"ErrorCode: {await task}");
+    }
+
+    static async Task<int> TestAsync(CancellationToken token, int delay)
+    {       
+      while (true)
+      {        
+        Thread.Sleep(delay);
+        if(token.IsCancellationRequested)
+        {
+          break;
+        }
+      }
+
+      return 299;
+    }
+  }
+}
